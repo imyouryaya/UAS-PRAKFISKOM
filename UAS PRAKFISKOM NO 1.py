eabfd19b0ea5b977b19b758b16af7397c33de523
imyouryaya <114723840+imyouryaya@users.noreply.github.com>
2022-12-27 17:47:17 +0700

    Add files via upload

---
+++ UAS PRAKFISKOM NO 1.py
@@ -0,0 +1,62 @@
+Python 3.11.0 (v3.11.0:deaf509e8f, Oct 24 2022, 14:43:23) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 2
+batas atas = 3
+jumlah grid = 100
+integral = 10.333350000000001
+
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 7
+batas atas = 10
+jumlah grid = 100
+integral = 231.00045
+
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 8
+batas atas = 20
+jumlah grid = 100
+integral = 2544.0288000000005
+
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 9
+batas atas = 10
+jumlah grid = 100
+integral = 94.33335
+>>> 
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 2
+batas atas = 15
+jumlah grid = 100
+integral = 1174.36995
+>>> 
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 20
+batas atas = 29
+jumlah grid = 100
+integral = 5499.012149999999
+>>> 
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 8
+batas atas = 10
+jumlah grid = 100
+integral = 170.66679999999997
+>>> 
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 2
+batas atas = 90
+jumlah grid = 100
+integral = 243360.6912
+>>> 
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 3
+batas atas = 28
+jumlah grid = 100
+integral = 7408.59375
+>>> 
+=========== RESTART: /Users/tiaravirginia/Documents/4.1. Contoh_1.py ===========
+batas bawah = 30
+batas atas = 33
+jumlah grid = 100
+integral = 2991.00045
